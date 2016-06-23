c513e688d0123aaf0ac9a36b69d423fe301411ee
Isaiah Mann <isaiah.mann@nick.com>
2016-06-22 17:29:12 -0700

    Initial Commit

---
+++ ISMCompressList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ISMCompressList
+//
+//  Created by Mann, Isaiah on 6/22/16.
+//  Copyright © 2016 Mann, Isaiah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
